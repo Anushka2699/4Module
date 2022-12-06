9476b578cc10d029b460086eba291af7eff3d743
Anushka2699 <108417245+Anushka2699@users.noreply.github.com>
2022-12-06 14:42:17 +0530

    update

---
+++ 4.js
@@ -0,0 +1,17 @@
+var names= new Array();
+names[0]="kanch";
+names[1]="janka";
+names[2]="james";
+names[3]="laks";
+names[4]="hack";
+names[5]="reps";
+names[6]="jeans";
+names[7]="jabmbo";
+
+for (var i=0; i<names.length; i++){
+    if(names[i].charAt(0)==='J'||names[i].charAt(0)==='j'){
+    console.log("Goodbey "+names[i])
+    }else{
+        console.log("Hello "+names[i])
+    }
+}
